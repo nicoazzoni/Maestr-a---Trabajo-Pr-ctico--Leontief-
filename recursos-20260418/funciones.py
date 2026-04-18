274daaa7b23c05a9a952aaf2358743a4e74b4754
nicoazzoni <nicolasazzoni@gmail.com>
2026-04-18 09:25:02 -0300

    Initial Commit

---
+++ recursos-20260418/funciones.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def calcularLeontief(A):
+    L = []
+    # su código
+    
+    ###########
+    return L
+
+def deltaProduccion(L, delta_d):
+    delta_p = []
+    # su código
+    
+    ###########
+    return delta_p
+
+
+def calcularCoeficientes(Z, totales):
+    A = []
+    # su código
+    
+    ###########
+    return A
+
+def calcularLeontiefInterregional(Arr, Ars, Ass, Asr):
+    L_ir = []
+    # su código
+    
+    ###########
+    return L_ir
+
+    
+    
+    
